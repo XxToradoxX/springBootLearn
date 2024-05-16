04a25b1e8a79825f06eb52c94f96607f8982d7cf
Administrator <1784264314@qq.com>
2024-05-16 16:05:52 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/config/SpringmvcConfig.java
@@ -0,0 +1,14 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringmvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+//        WebMvcConfigurer.super.addViewControllers(registry);
+        registry.addViewController("/").setViewName("forward:nb.html");
+    }
+}
