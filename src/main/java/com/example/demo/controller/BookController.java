f4e47a89dd24b2bf204475f81798052cca062a1a
XxToradoxX <1784264314@qq.com>
2024-05-19 11:10:44 +0800

    6666

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import com.example.demo.pojo.Book;
 import com.example.demo.pojo.Result;
 import com.example.demo.service.BookService;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@Slf4j
 @RequestMapping("/book")
 public class BookController {
     @Autowired
@@ -19,6 +22,7 @@ public class BookController {
     @PostMapping()
     public Result bookList() {
         List<Book> bookList = bookService.SelectAll();
+        log.info("{}", bookList);
         return Result.success(bookList);
     }
 }
