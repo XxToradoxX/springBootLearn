1e0841e37132de3b0af766876649bc0b66b4e39d
XxToradoxX <1784264314@qq.com>
2024-05-21 20:36:06 +0800

    6666

--- sql/demo.sql
+++ sql/demo.sql
@@ -2,8 +2,7 @@
 SQLyog Ultimate v12.09 (64 bit)
 MySQL - 5.7.41-log : Database - abc
 *********************************************************************
-*/
-
+*/
 
 /*!40101 SET NAMES utf8 */;
 
@@ -26,11 +25,38 @@ CREATE TABLE `book` (
   `BookName` varchar(50) DEFAULT NULL,
   `BookCode` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`BookId`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 /*Data for the table `book` */
 
-insert  into `book`(`BookId`,`BookName`,`BookCode`) values (1,'逸一时误一世','1919810'),(4,'我爱你dcc','11111');
+insert  into `book`(`BookId`,`BookName`,`BookCode`) values (1,'逸一时误一世','1919810'),(4,'张三','11111'),(5,'张三','11111'),(6,'张三','11111'),(7,'张三','11111'),(8,'张三','11111'),(9,'张三','11111');
+
+/*Table structure for table `family` */
+
+DROP TABLE IF EXISTS `family`;
+
+CREATE TABLE `family` (
+  `family_id` int(11) NOT NULL AUTO_INCREMENT,
+  `family_name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`family_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `family` */
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(50) DEFAULT NULL,
+  `family_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  KEY `family_id` (`family_id`),
+  CONSTRAINT `user_ibfk_1` FOREIGN KEY (`family_id`) REFERENCES `family` (`family_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `user` */
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
