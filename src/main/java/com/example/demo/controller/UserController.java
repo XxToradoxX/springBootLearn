84bd23413022c265d2d0073e75ecf05fd4141a5a
POPOGI <lre20666@163.com>
2024-05-22 09:37:17 +0800

    666

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,11 +4,9 @@ import com.example.demo.pojo.Result;
 import com.example.demo.pojo.User;
 import com.example.demo.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.annotations.Delete;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,7 +23,7 @@ public class UserController {
         return Result.success(userList);
     }
 
-    @PostMapping("/{familyId}")
+    @DeleteMapping("/{familyId}")
     public Result DeleteFamilyByIdAndAllItsUsers(@PathVariable("familyId") Integer familyId) {
         Integer flag = userService.DeleteFamilyByIdAndAllItsUsers(familyId);
         if(flag != null){
