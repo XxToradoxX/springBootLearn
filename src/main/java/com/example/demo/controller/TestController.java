ce56b641d97b899819aeb70c1b58955a4fd7ed77
XxToradoxX <1784264314@qq.com>
2024-05-26 13:06:56 +0800

    6666

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -42,6 +42,7 @@ public class TestController {
         }
         try {
 //            System.out.println(System.getProperty("user.dir")+"\\src");
+//            String uploadDir = "D:/";
             byte[] bytes = file.getBytes();
             String uploadDir = System.getProperty("user.dir")+"\\src\\main\\java\\com\\example\\demo\\file\\";
             File uploadedFile = new File(uploadDir + file.getOriginalFilename());
