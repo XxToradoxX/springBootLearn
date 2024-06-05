29a1483c490f974bbf7be165783da6611edb580f
Administrator <1784264314@qq.com>
2024-06-05 09:48:05 +0800

    666

---
+++ Boot-Parent/springBootLearn/sql/demo.sql
@@ -0,0 +1,68 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 5.7.41-log : Database - abc
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`abc` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `abc`;
+
+/*Table structure for table `book` */
+
+DROP TABLE IF EXISTS `book`;
+
+CREATE TABLE `book` (
+  `BookId` int(11) NOT NULL AUTO_INCREMENT,
+  `BookName` varchar(50) DEFAULT NULL,
+  `BookCode` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`BookId`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+/*Data for the table `book` */
+
+insert  into `book`(`BookId`,`BookName`,`BookCode`) values (1,'逸一时误一世','1919810'),(4,'张三','11111'),(5,'张三','11111'),(6,'张三','11111'),(7,'张三','11111'),(8,'张三','11111'),(9,'张三','11111');
+
+/*Table structure for table `family` */
+
+DROP TABLE IF EXISTS `family`;
+
+CREATE TABLE `family` (
+  `family_id` int(11) NOT NULL AUTO_INCREMENT,
+  `family_name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`family_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `family` */
+
+insert  into `family`(`family_id`,`family_name`) values (1,'familyA'),(2,'familyB');
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(50) DEFAULT NULL,
+  `family_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  KEY `family_id` (`family_id`),
+  CONSTRAINT `user_ibfk_1` FOREIGN KEY (`family_id`) REFERENCES `family` (`family_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `user` */
+
+insert  into `user`(`user_id`,`user_name`,`family_id`) values (1,'lisi',1),(3,'zhangsan',2);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
